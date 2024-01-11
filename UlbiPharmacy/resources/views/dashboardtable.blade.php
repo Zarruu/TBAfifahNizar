b495e684e0df1c2c85598cad53ffa2bd8a7104ff
Zarruu <nizarakmal8@gmail.com>
2024-01-11 11:14:56 +0700

    update be1

--- UlbiPharmacy/resources/views/dashboardtable.blade.php
+++ UlbiPharmacy/resources/views/dashboardtable.blade.php
@@ -8,27 +8,29 @@
     <table style="background-color: aliceblue">
         <thead>
             <tr>
-                <th>PID</th>
-                <th>Nama Penyakit</th>
+                <th>ID</th>
+                <th>Nama Obat</th>
                 <th>Gejala</th>
                 <th></th>
             </tr>
         </thead>
         <tbody>
+            <?php $i =1; ?>
+            @foreach ($nama_obat as $no)
+                
+            
             <tr>
-                <td>1</td>
-                <td>Flu</td>
-                <td>demam, pilek, radang</td>
-                <td>
-                    <a href="/login">
-                        <button class="button_delete">
-                            DELETE
-                        </button>
-                        <button class= "button_edit">
-                            EDIT
-                        </button>
-                    </a>
+                <td><?= $i ?></td>
+                {{-- <td>{!! $no->nama!!}</td> --}}
+                <td>{!! $no->nama!!}</td>
+                <td>{!! $no->gejala!!}</td>
+                <td>    
+                    <a href="{{ route('edit.medicine')}}" class="btn btn-warning btn-sm">Edit</a>
+                    <a href="{{ route('delete.medicine')}}"class="btn btn-danger btn-sm">Del</a>
                 </td>
+            </tr>
+            <?php $i++; ?>
+            @endforeach
             </tr>
             <tr>
                 <td>2</td>
